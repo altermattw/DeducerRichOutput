140957f3dddf5d17e6cc79eb4cb05c925625f6bb
altermattw <altermattw@hanover.edu>
2013-05-07 13:06:45 -0400

    Reorganizing to separate repos
    Removed all packages not related directly to DeducerRichOutput and put
    them in their own repos.

--- DeducerANOVA/R/output.R
+++
@@ -1,5 +0,0 @@
-output <- function(lis) {
-	for(i in 1:length(lis)) {
-		ANOVAd$addTab(names(lis)[i],h.df(lis[[i]]))
-		}
-	}
--- DeducerANOVA/R/splitcol.R
+++
@@ -1,6 +0,0 @@
-splitcol <- function(variable,split,newnames) {
-	tmp <- strsplit(as.character(variable),split)
-	newvars <- data.frame(t(sapply(tmp,c)))
-	names(newvars) <- newnames
-	return(newvars) 
-	}
